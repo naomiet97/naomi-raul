664f445d69755171759a0b4cce4f429519ab697c
Raúl Sendín <raulsendin13@gmail.com>
2023-05-25 17:30:45 +0200

    starting

--- katas/01-hello-world/kata.js
+++ katas/01-hello-world/kata.js
@@ -1,11 +1,3 @@
 export function kata() {
-  const div = document.createElement("div");
-  div.id = "square";
-  const p = document.createElement("p");
-  p.id = "text";
-  p.textContent = "Hello World";
-
-  div.appendChild(p);
-
-  document.body.appendChild(div);
+  // TODO!
 }
--- katas/02-query-selector/kata.js
+++ katas/02-query-selector/kata.js
@@ -1,4 +1,3 @@
 export function kata() {
-  const starWars = document.querySelector("main > section:nth-child(2) > p");
-  starWars.classList.add("star-wars");
+  // TODO!
 }
