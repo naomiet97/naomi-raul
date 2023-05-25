7018b890c4a4ed3a7ca5b5f975ee9eb2187e37e9
Naomi Elvira <naomiet97@gmail.com>
2023-05-25 18:00:03 +0200

    Actualizo repo. Naomi

--- katas/01-hello-world/kata.js
+++ katas/01-hello-world/kata.js
@@ -1,3 +1,9 @@
 export function kata() {
-  // TODO!
+  const div = document.createElement("div");
+  div.id = "square";
+  const p = document.createElement("p");
+  p.id = "text";
+  p.textContent = "Hello World";
+  div.appendChild(p);
+  document.body.appendChild(div);
 }
